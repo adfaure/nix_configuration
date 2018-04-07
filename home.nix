498b1c4a08528245cd91b0ac7cd98018cb671641
adfaure <adrien.faure@protonmail.com>
2018-04-07 19:13:15 +0200

    add home configuration

--- home.nix
+++ home.nix
@@ -6,11 +6,21 @@ in
 mkHome rec {
   user = "adfaure";
   files = {
+
 	 ".tmux.conf" = "/home/${user}/dotfiles/files/tmux.conf";
+
+   # I don't know if I need this as I have my_vim.nix
    # ".vimrc" = "/home/${user}/dotfiles/files/vimrc";
+
 	 ".bashrc".content = ''
 	 echo use ZSH you moron!!
 	 '';
+
+   # zsh config
+   ".zshrc" = "/home/${user}/dotfiles/files/zshrc";
+   ".zshrc.local" = "/home/${user}/dotfiles/files/zshrc.local";
+
+   "config/sakura/sakura.conf" = "/home/${user}/dotfiles/files";
   };
 }
 
