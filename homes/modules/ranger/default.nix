d524e3c73f4987eb93bca8e7ed7e1bf2b46f2b6e
Faure Adrien <adrien.faure@protonmail.com>
2021-02-03 13:08:19 +0100

    home: fix ranger default editor

--- homes/modules/ranger/default.nix
+++ homes/modules/ranger/default.nix
@@ -2,17 +2,19 @@
   home.packages = [ pkgs.ranger pkgs.atool pkgs.poppler_utils ];
 
   # environment.shellAliases = { ranger = "ranger --confdir=$HOME/.ranger"; };
+  programs.zsh = {
+    initExtra = lib.mkAfter ''
+      # Add ctrl+N shortcut to navigate with ranger and zsh
+      _ranger () {
+        PYTHONPATH= command ${pkgs.ranger}/bin/ranger --confdir=$HOME/.config/ranger "$(pwd)"<$TTY
+        print -n "\033[A"
+        zle && zle -I
+        cd "$(grep \^\' $HOME/.config/ranger/bookmarks | cut -b3-)"
+      }
 
-  programs.zsh.initExtra = lib.mkAfter ''
-    # Add ctrl+N shortcut to navigate with ranger and zsh
-    _ranger () {
-      PYTHONPATH= command ${pkgs.ranger}/bin/ranger --confdir=$HOME/.ranger "$(pwd)"<$TTY
-      print -n "\033[A"
-      zle && zle -I
-      cd "$(grep \^\' $HOME/.ranger/bookmarks | cut -b3-)"
-    }
-
-    zle -N _ranger
-    bindkey -v '^N' _ranger
-  '';
+      zle -N _ranger
+      bindkey -v '^N' _ranger
+    '';
+    sessionVariables = { EDITOR = "v"; };
+  };
 }
