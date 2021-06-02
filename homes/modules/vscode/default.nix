95141f1c99668241677dc7b2e6d9b159bf771e3f
Faure Adrien <adrien.faure2@gmail.com>
2021-06-02 14:50:38 +0200

    nixos: dns config

--- homes/modules/vscode/default.nix
+++ homes/modules/vscode/default.nix
@@ -1,12 +1,24 @@
 { config, lib, pkgs, my-dotfiles, ... }:
 let
   extensions = (with pkgs.vscode-extensions; [
+    # Languages support
     bbenoist.Nix
     ms-python.python
+    dhall.dhall-lang
+
+    # Themes
+    pkief.material-icon-theme
+
+    # Tools
+    streetsidesoftware.code-spell-checker
+    # ms-vscode-remote.remote-ssh
     ms-azuretools.vscode-docker
-    ms-vscode-remote.remote-ssh
+    eamodio.gitlens
+
+    # Vim mode
     vscodevim.vim
   ]) ++ pkgs.vscode-utils.extensionsFromVscodeMarketplace [
+    # Languages support
     {
       name = "org-mode";
       publisher = "tootone";
@@ -25,7 +37,37 @@ let
       version = "0.7.0";
       sha256 = "sha256-QPO5IA5mrYo6cn3hdTjmzhbRN/YU7G4yMspJ+dRBx5o=";
     }
-
+    {
+      name = "mypy";
+      publisher = "matangover";
+      version = "0.1.5";
+      sha256 = "sha256-/cI/fj4gtm/eTiNN48WPnpq5c3Led0Azi24YWO4BowE=";
+    }
+    # Theme and appearance
+    {
+      name = "non-breaking-space-highlighter";
+      publisher = "viktorzetterstrom";
+      version = "0.0.3";
+      sha256 = "sha256-t+iRBVN/Cw/eeakRzATCsV8noC2Wb6rnbZj7tcZJ/ew=";
+    }
+    {
+      name = "trailing-spaces";
+      publisher = "shardulm94";
+      version = "0.3.1";
+      sha256 = "sha256-aCFtiONPjKewzOl7cRoQegU2J/6+5Cbn2ezgXF79YEA=";
+    }
+    {
+      name = "trailing-spaces";
+      publisher = "shardulm94";
+      version = "0.3.1";
+      sha256 = "sha256-aCFtiONPjKewzOl7cRoQegU2J/6+5Cbn2ezgXF79YEA=";
+    }
+    {
+      publisher = "vscode-icons-team";
+      name = "vscode-icons";
+      version = "11.4.0";
+      sha256 = "sha256-SZsGot8qTmzqOeP2KUga/tb2vUmyugz4uizRR0qxvn0=";
+    }
   ];
 
   vscode-with-extensions =
