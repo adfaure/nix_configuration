227de2826fc43bc8cab9c10fa7cb397db631753d
Faure Adrien <adrien.faure2@gmail.com>
2021-06-11 19:48:15 +0200

    nixos: add cachix module

---
+++ nixos/modules/cachix/caches/capack.nix
@@ -0,0 +1,10 @@
+{
+  nix = {
+    binaryCaches = [
+      "https://capack.cachix.org"
+    ];
+    binaryCachePublicKeys = [
+      "capack.cachix.org-1:38D+QFk3JXvMYJuhSaZ+3Nm/Qh+bZJdCrdu4pkIh5BU="
+    ];
+  };
+}
