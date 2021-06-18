183b2903ca58445fb5979a24bcde99b320149fd2
Faure Adrien <adrien.faure2@gmail.com>
2021-06-18 23:04:40 +0200

    CI: start ci

--- flake.nix
+++ flake.nix
@@ -8,8 +8,8 @@
     # Needed to have a recent hugo version for the kodama package
     nixos-unstable.url = "github:NixOS/nixpkgs/nixos-unstable";
     my-dotfiles = {
-      # url = "github:/adfaure/dotfiles";
-      url = "/home/adfaure/Projects/dotfiles";
+      url = "github:/adfaure/dotfiles";
+      # url = "/home/adfaure/Projects/dotfiles";
       # It is possible to pin the revision with:
       # To be fully reproducible, I can pin my repos
       # "github:/adfaure/dotfiles?rev=602790e25de91ae166c10b93735bbaea667f7a49";
@@ -140,7 +140,8 @@
 
       # Sanity check for deploy-rs
       checks = {
-        # cgvg = self.packages.x86_64-linux.cgvg;
+        cgvg = self.packages.x86_64-linux.cgvg;
+        kodama = self.packages.x86_64-linux.cgvg;
       };
 
       # You can enter this shell with `nix develop`
