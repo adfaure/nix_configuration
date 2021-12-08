c1d5e6b99014fb5726cad7e93ccefa11a5b09a5e
Faure Adrien <adrien.faure2@gmail.com>
2021-12-08 15:56:57 +0100

    home: remove useless package

--- homes/modules/emacs/default.nix
+++ homes/modules/emacs/default.nix
@@ -1,5 +1,5 @@
 { config, lib, pkgs, my-dotfiles, wrapCmd, ... }:
 {
-  home.packages = [ pkgs.spotify ];
+  home.packages = [ ];
   home.file.".emacs".text = builtins.readFile "${my-dotfiles}/files/emacs_conf";
 }
