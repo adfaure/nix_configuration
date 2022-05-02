5f2ce5dd663261fe9a599a8673b564b1b050aee1
Adrien Faure <adrien.faure@protonmail.com>
2022-05-02 10:51:44 +0200

    profile: azerty restored

--- flake.nix
+++ flake.nix
@@ -6,8 +6,8 @@
     # Needed to have a recent hugo version for the kodama package
     nixos-unstable.url = "github:NixOS/nixpkgs/nixos-unstable";
     my-dotfiles = {
-      # url = "github:/adfaure/dotfiles/azerty";
-      url = "/home/adfaure/Projects/dotfiles";
+      url = "github:adfaure/dotfiles";
+      # url = "/home/adfaure/Projects/dotfiles";
       # It is possible to pin the revision with:
       # To be fully reproducible, I can pin my repos
       # "github:/adfaure/dotfiles?rev=602790e25de91ae166c10b93735bbaea667f7a49";
