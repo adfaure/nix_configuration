6544548ba4c00db0a19f9ee862db28adb766688c
Adrien Faure <adrien.faure@protonmail.com>
2022-06-14 10:21:22 +0200

    home: aspell with fr in emacs

--- homes/modules/emacs/default.nix
+++ homes/modules/emacs/default.nix
@@ -1,5 +1,9 @@
 { config, lib, pkgs, my-dotfiles, wrapCmd, ... }:
 {
-  home.packages = [ pkgs.myEmacs ];
+  home.packages = [
+    pkgs.myEmacs
+    (pkgs.aspellWithDicts (d: [d.fr d.en]))
+  ];
+  home.file.".aspell.conf".text = "data-dir ${pkgs.aspell}/lib/aspell";
   home.file.".emacs".text = builtins.readFile "${my-dotfiles}/files/emacs_conf";
 }
