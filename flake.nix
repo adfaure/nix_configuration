63b4d8e702866882ca5c55a5cfcb13f1fefad9da
Adrien Faure <adrien.faure@protonmail.com>
2022-07-21 11:35:00 +0200

    kodama: remove vps configuration

--- flake.nix
+++ flake.nix
@@ -3,7 +3,6 @@
   inputs = {
     # I need a custom nix version because of this issue: https://github.com/NixOS/nix/commit/8af4f886e212346afdd1d40789f96f1321da96c5
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-22.05";
-    # Needed to have a recent hugo version for the kodama package
     nixos-unstable.url = "github:NixOS/nixpkgs/nixos-unstable";
     my-dotfiles = {
       url = "github:adfaure/dotfiles";
@@ -15,19 +14,13 @@
     };
     home-manager.url = "github:nix-community/home-manager";
     home-manager.inputs.nixpkgs.follows = "nixos-unstable";
-    # Deployments tool for my personnal web server
-    deploy-rs.url = "github:serokell/deploy-rs";
-    # Secret management with sops
-    sops-nix.url = "github:Mic92/sops-nix";
     # nur-kapack = { url = "/home/adfaure/Projects/nur-kapack"; };
     nur.url = "github:nix-community/NUR";
     # Emacs overlay
     emacs-overlay.url = "github:nix-community/emacs-overlay";
-    kodama.url = "github:adfaure/kodama";
   };
 
-  outputs = inputs@{ self, nixpkgs, nixos-unstable, my-dotfiles, deploy-rs
-  , sops-nix, home-manager, emacs-overlay, nur, kodama }:
+  outputs = inputs@{ self, nixpkgs, nixos-unstable, my-dotfiles, home-manager, emacs-overlay, nur }:
   let
         pkgs = import nixpkgs {
           system = "x86_64-linux";
@@ -43,9 +36,6 @@
   {
 
       packages.x86_64-linux = {
-        # Dedicated package for my personal website
-        kodama = kodama.packages.x86_64-linux.website;
-        batsite = pkgs.callPackage ./pkgs/batsite { };
         # Programs
         cgvg = pkgs.callPackage ./pkgs/cgvg { };
         myVscode = unstable.callPackage ./pkgs/vscode { };
@@ -137,75 +127,10 @@
             ./deployments/configuration-adchire.nix
           ];
         };
-
-        # Configuration for my website server, it is supposed to be deployed with deploy-rs tool.
-        # I for the moment I rely on unstable as home-manager uses features not yet available in nixos-20.09
-        kodama = nixpkgs.lib.nixosSystem {
-          system = "x86_64-linux";
-          # extra arguments will be injected into the modules.s
-          extraArgs = {
-            inherit my-dotfiles emacs-overlay;
-            cgvg = self.packages.x86_64-linux.cgvg;
-            kodama = self.packages.x86_64-linux.kodama;
-            batsite = self.packages.x86_64-linux.batsite;
-          };
-          modules = [
-            self.nixosModules.overlay
-
-            # Main configuration, includes the hardware file and the module list.
-            ./deployments/configuration-kodama.nix
-            # Install sops-nix.
-            sops-nix.nixosModules.sops
-            # Secrets configuration with sops.
-            ({ config, ... }: {
-              # The default file containing the secrets.
-              sops.defaultSopsFile = ./secrets.yaml;
-              # We tell sops-nix which secrets we use.
-              sops.secrets.radicaleUsers = { };
-              # This part is required to enable nginx to access to the decrypted file.
-              systemd.services.nginx = {
-                serviceConfig.SupplementaryGroups =
-                  [ config.users.groups.keys.name ];
-              };
-              sops.secrets.radicaleUsers.owner = config.users.users.nginx.name;
-            })
-            # Deploy the home-manager configuration with the home manager nixos module.
-            # This enable to be set in a complete working environment when I am logging to new machines.
-            home-manager.nixosModules.home-manager
-            ./nixos/profiles/home-manager/base.nix
-          ];
-        };
       };
-
-      # This enable to bind the deploy-rs app to my flake.
-      # This way I wont have version conflicts. Use with `nix run .#deploy-rs`
-      apps.x86_64-linux.deploy-rs = deploy-rs.apps.x86_64-linux.deploy-rs;
-
-      # deploy-rs configuration to deploy kodama.
-      deploy.nodes.kodama = {
-        profiles = {
-          system = {
-            sshUser = "adfaure";
-            path = deploy-rs.lib.x86_64-linux.activate.nixos
-              self.nixosConfigurations.kodama;
-            # This suppose that the ssh user can use sudo without password.
-            user = "root";
-          };
-        };
-        # The at this hostname must be running NixOS.
-        hostname = "adrien-faure.fr";
-        fastConnection = true;
-      };
-
-      # Sanity check for deploy-rs
+      
       checks.x86_64-linux = with import nixpkgs { system = "x86_64-linux"; }; {
         cgvg = self.packages.x86_64-linux.cgvg;
-        build-kodama = self.packages.x86_64-linux.kodama;
-
-        # Deploy-rs sanity check
-        inherit (builtins.mapAttrs
-          (system: deployLib: deployLib.deployChecks self.deploy) deploy-rs.lib)
-        ;
       };
-    };
+  };
 }
