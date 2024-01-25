4f5bbd6e99d553d68fa930abcc886e4787a56f76
Adrien Faure <adrien.faure@protonmail.com>
2024-01-26 00:15:32 +0100

    nixos: better separation for graphical sevices

--- nixos/profiles/graphical/default.nix
+++ nixos/profiles/graphical/default.nix
@@ -5,38 +5,17 @@
   pi3blocksi3blockskgs,
   ...
 }: let
-  sddm-theme-chili = pkgs.stdenv.mkDerivation rec {
-    name = "sddm-chili";
 
-    src = pkgs.fetchFromGitHub {
-      owner = "MarianArlt";
-      repo = name;
-      rev = "6516d50176c3b34df29003726ef9708813d06271";
-      sha256 = "sha256-wxWsdRGC59YzDcSopDRzxg8TfjjmA3LHrdWjepTuzgw=";
-    };
-
-    installPhase = ''
-      mkdir $out/share/sddm/themes/${name} -p
-      cp ${src}/* $out/share/sddm/themes/${name}/. -aR
-    '';
-
-    meta = with lib; {
-      description = "Theme for SDDM";
-      homepage = "https://github.com/MarianArlt/sddm-chili";
-      license = licenses.gpl3Only;
-      maintainers = with maintainers; [dan4ik605743];
-      platforms = platforms.linux;
-    };
-  };
 in
   with lib; {
     imports = [
       ./packages_list.nix
       ../../services/sway
+      ../../services/gnome
     ];
 
     environment.adfaure.environments.graphical.enable = true;
-    environment.adfaure.services.sway.enable = true;
+    environment.adfaure.services.gnome.enable = true;
 
     # environment.adfaure.programs.emacs.enable=true;
     programs.light.enable = true;
@@ -57,38 +36,9 @@ in
     services.pipewire = {
       enable = true;
       alsa.enable = true;
+      alsa.support32Bit = true;
       pulse.enable = true;
     };
-
-    services.xserver = {
-      enable = true;
-      displayManager.sddm = {
-        enable = true;
-        theme = "sddm-chili";
-        enableHidpi = true;
-      };
-
-      layout = "fr";
-      # xkbVariant = "bepo";
-      resolutions = [
-        {
-          x = 2560;
-          y = 1440;
-        }
-        {
-          x = 1920;
-          y = 1080;
-        }
-      ];
-      libinput.enable = true;
-    };
-
-    environment.systemPackages = with pkgs; [
-      # SDDM Theme
-      libsForQt5.plasma-framework
-      libsForQt5.qt5.qtgraphicaleffects
-      sddm-theme-chili
-    ];
 
     services.dbus.enable = true;
 
