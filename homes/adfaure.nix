9ff9a6321135b5deb9db97fdb298ccbb8769beef
Adrien Faure <adrien.faure@protonmail.com>
2024-01-29 10:17:23 +0100

    home: add workaround for obsidian

--- homes/adfaure.nix
+++ homes/adfaure.nix
@@ -89,6 +89,17 @@
       spotify
       signal-desktop
       rofi
+
+      # Using this for the moment ... https://github.com/NixOS/nixpkgs/issues/273611
+      (lib.throwIf (lib.versionOlder "1.4.16" pkgs.obsidian.version) "Obsidian no longer requires EOL Electron" (
+        pkgs.obsidian.override {
+          electron = pkgs.electron_25.overrideAttrs (_: {
+            preFixup = "patchelf --add-needed ${pkgs.libglvnd}/lib/libEGL.so.1 $out/bin/electron"; # NixOS/nixpkgs#272912
+            meta.knownVulnerabilities = []; # NixOS/nixpkgs#273611
+          });
+        }
+      ))
+
     ];
   };
 }
