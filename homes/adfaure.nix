98e40f4775c5c707ae958fb16dae093b3756af06
Adrien Faure <adrien.faure@protonmail.com>
2024-03-08 22:32:19 +0100

    nix flake udpate

--- homes/adfaure.nix
+++ homes/adfaure.nix
@@ -104,7 +104,7 @@
       nvtop
       discord
       # better discord ? at least can screen share with gnome on wayland
-      vesktop
+      unstable.vesktop
       # Using this for the moment ... https://github.com/NixOS/nixpkgs/issues/273611
       (lib.throwIf (lib.versionOlder "1.4.16" pkgs.obsidian.version) "Obsidian no longer requires EOL Electron" (
         pkgs.obsidian.override {
