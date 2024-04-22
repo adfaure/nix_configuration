ae05eff30c77a43fb5a1863dd862cb1dbab885b9
Adrien Faure <adrien.faure@protonmail.com>
2024-04-22 10:59:19 +0200

    home: vim autocomp and ale

--- homes/modules/tmux/default.nix
+++ homes/modules/tmux/default.nix
@@ -13,8 +13,8 @@ in {
     extraConfig =
       builtins.readFile "${my-dotfiles}/files/tmux"
       + ''
-        bind-key C-h display-popup -E -h 75% -w 75% -d "#{pane_current_path}" "htop"
-        bind-key C-s display-popup -E -h 75% -w 75% -d "#{pane_current_path}" "(tmux new-session -s scratch -d && tmux new-window -t scratch:4 'emacs -nw ~/Projects/Notes/Journal.org'); tmux a -t scratch:1"
+        bind-key C-h display-popup -E -h 75% -w 75% -d "#{pane_current_path}" "${pkgs.zenith}/bin/zenith"
+        bind-key C-s display-popup -E -h 75% -w 75% -d "#{pane_current_path}" "(tmux new-session -s scratch -d && tmux new-window -t scratch:4 'cd ~/Vault; vim'); tmux a -t scratch:1"
 
         # bind-key C-p display-popup -E -h 75% -w 75% -d "#{pane_current_path}" "tmux attach -t scratch:2"
         # bind-key C-r display-popup -E -h 75% -w 75% -d "#{pane_current_path}" "tmux attach -t scratch:3"
--- homes/modules/vim/default.nix
+++ homes/modules/vim/default.nix
@@ -3,9 +3,10 @@
   lib,
   pkgs,
   my-dotfiles,
+  unstable,
   ...
 }: let
-  my_vim_plugins = pkgs.callPackage ./my_vim_plugins.nix {};
+  my_vim_plugins = pkgs.callPackage ./my_vim_plugins.nix {inherit unstable;};
 in {
   home.packages =
     [
@@ -24,6 +25,7 @@ in {
     # withPython3 = true;
     # https://github.com/nix-community/home-manager/issues/1712
     extraConfig = builtins.readFile "${my-dotfiles}/files/vimrc";
+    extraLuaConfig = builtins.readFile ./init.lua;
     plugins = my_vim_plugins.plugins;
     # extraPackages = with pkgs;
     #   [ (python3.withPackages (ps: with ps; [ black flake8 jedi ])) rnix-lsp ]
