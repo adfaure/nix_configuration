22cbbae281ff53b120bb5e565adc5a8ac9ea2829
Adrien Faure <adrien.faure@protonmail.com>
2024-07-16 14:11:31 +0200

    home: tmux-vim pane navigator works with poetry shell

--- homes/modules/vim-tmux-panes/default.nix
+++ homes/modules/vim-tmux-panes/default.nix
@@ -14,8 +14,17 @@ in {
     programs.tmux.extraConfig = ''
       # Smart pane switching with awareness of Vim splits.
       # See: https://github.com/christoomey/vim-tmux-navigator
-      is_vim="ps -o state= -o comm= -t '#{pane_tty}' \
-          | grep -iqE '^[^TXZ ]+ +(\\S+\\/)?g?(view|l?n?vim?x?|fzf)(diff)?$'"
+      # is_vim="ps -o state= -o comm= -t '#{pane_tty}' \
+      #     | grep -iqE '^[^TXZ ]+ +(\\S+\\/)?g?(view|l?n?vim?x?|fzf)(diff)?$'"
+
+      # https://github.com/christoomey/vim-tmux-navigator/issues/295#issuecomment-1369954705
+      # Enable tmux pan in subshell (such as poetry shell)
+      is_vim="children=(); i=0; pids=( $(ps -o pid= -t '#{pane_tty}') ); \
+        while read -r c p; do [[ -n c && c -ne p && p -ne 0 ]] && children[p]+=\" $\{c\}\"; done <<< \"$(ps -Ao pid=,ppid=)\"; \
+        while (( $\{#pids[@]\} > i )); do pid=$\{pids[i++]\}; pids+=( $\{children[pid]-\} ); done; \
+        ps -o state=,comm= -p \"$\{pids[@]\}\" | grep -iqE '^[^TXZ ]+ +(\\S+\\/)?g?(view|n?vim?x?)(diff)?$'"
+
+
       bind-key -n 'C-h' if-shell "$is_vim" 'send-keys C-h'  'select-pane -L'
       bind-key -n 'C-j' if-shell "$is_vim" 'send-keys C-j'  'select-pane -D'
       bind-key -n 'C-k' if-shell "$is_vim" 'send-keys C-k'  'select-pane -U'
