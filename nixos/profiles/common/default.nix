1cc3fb2d6fecbc987641e19340012a399ab6e1cf
Adrien Faure <adrien.faure@protonmail.com>
2025-02-14 00:23:45 +0100

    test: residual restic nixos service

--- nixos/profiles/common/default.nix
+++ nixos/profiles/common/default.nix
@@ -12,12 +12,10 @@
     ../../modules/cachix
     # Simple guix module with guix sevice enabled and package added to env
     ../../modules/guix
-    ../../services/nix-sops
   ];
 
   adfaure.modules.my-guix.enable = true;
   adfaure.services.syncthing.enable = true;
-  adfaure.services.nix-sops.enable = true;
 
   # use Vim by default
   environment.shellAliases = {
