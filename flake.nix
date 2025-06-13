318d1062bb97b7776e0dac468b93c91eccb1942e
Adrien Faure <adrien.faure@protonmail.com>
2025-06-13 22:48:38 +0200

    flake: update

--- flake.nix
+++ flake.nix
@@ -13,7 +13,7 @@
       flake = false;
     };
     home-manager = {
-      url = "github:nix-community/home-manager";
+      url = "github:nix-community/home-manager/release-25.05";
       inputs.nixpkgs.follows = "nixpkgs";
     };
     nixvim-config = {
