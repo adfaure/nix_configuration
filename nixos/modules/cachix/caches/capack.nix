814944cf335d766464b86db2f73b6144d2723de9
Adrien Faure <adrien.faure@protonmail.com>
2025-06-19 17:27:16 +0200

    nixos: add nixpkgs-review-gha cache settings

--- nixos/modules/cachix/caches/capack.nix
+++ nixos/modules/cachix/caches/capack.nix
@@ -2,9 +2,11 @@
   nix = {
     settings.substituters = [
       "https://capack.cachix.org"
+      "https://nixpkgs-review-gha.cachix.org"
     ];
-    # settings.trusted-plublic-keys = [
-    #   "capack.cachix.org-1:38D+QFk3JXvMYJuhSaZ+3Nm/Qh+bZJdCrdu4pkIh5BU="
-    # ];
+    settings.trusted-public-keys = [
+      "capack.cachix.org-1:38D+QFk3JXvMYJuhSaZ+3Nm/Qh+bZJdCrdu4pkIh5BU="
+      "nixpkgs-review-gha.cachix.org-1:6R3LPTtyfgo4pJASCKrU3iUUrrxVKjW5+Ot9S5SWeKM="
+    ];
   };
 }
