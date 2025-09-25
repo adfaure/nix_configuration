b5be5c82fe7e406b44815531229afd6e4f0069cc
Adrien Faure <adrien.faure@protonmail.com>
2025-09-25 23:37:16 +0200

    nixos: function to build system

--- nixos/systems/noco.nix
+++ nixos/systems/noco.nix
@@ -3,13 +3,14 @@
   system,
   ...
 }: let
-  inherit (inputs) my-dotfiles nixpkgs self;
+  inherit (inputs) my-dotfiles nixpkgs determinate self;
 in
   nixpkgs.lib.nixosSystem {
     inherit system;
     specialArgs = {inherit my-dotfiles;};
     modules = [
       self.nixosModules.overlay
+      determinate.nixosModules.default
       # Main configuration, includes the hardware file and the module list
       ../deployments/configuration-noco.nix
     ];
