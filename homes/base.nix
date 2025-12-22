9e8edc6184c15cfc940ec9790b9ed94c2f8b01c0
Adrien Faure <adrien.faure@protonmail.com>
2025-12-22 08:56:43 +0100

    home: upgrade git settings

--- homes/base.nix
+++ homes/base.nix
@@ -34,26 +34,21 @@
     my-programs.nushell.enable = true;
 
     # Small git config (should I make a dedicated module?)
+    programs.difftastic.git.enable = true;
     programs.git = {
       enable = true;
-      difftastic.enable = true;
-      userName = "Adrien Faure";
-      userEmail = "adrien.faure@protonmail.com";
-      aliases = {
-        ds = "diff --staged";
-        l = "log --pretty=format:'%C(auto,yellow)%h%C(auto,magenta) %C(auto,blue)%ad %C(auto,green)%aN %C(auto,reset)%s%C(auto)% gD%d' --graph --date=format:'%Y-%m-%d %H:%M:%S' --decorate-refs-exclude='refs/remotes/*/HEAD'";
-        st = "status";
-        chk = "checkout";
-      };
-      extraConfig = {
-        # init = {
-        #   defaultBranch = "master";
-        # };
-        # push.default = "current";
-        # safe.bareRepository = "explicit";
-        # rebase.instructionFormat = "%d %s";
+      settings = {
+        user = {
+          email = "adrien.faure@protonmail.com";
+          name = "Adrien Faure";
+        };
+        alias = {
+          ds = "diff --staged";
+          l = "log --pretty=format:'%C(auto,yellow)%h%C(auto,magenta) %C(auto,blue)%ad %C(auto,green)%aN %C(auto,reset)%s%C(auto)% gD%d' --graph --date=format:'%Y-%m-%d %H:%M:%S' --decorate-refs-exclude='refs/remotes/*/HEAD'";
+          st = "status";
+          chk = "checkout";
+        };
         merge.conflictstyle = "diff3";
-        # commit.verbose = true;
       };
     };
 
@@ -80,7 +75,6 @@
 
     programs.ssh = {
       enable = true;
-      # extraConfig = builtins.readFile "${my-dotfiles}/files/ssh_config";
     };
 
     programs.bash = {
