c873a57b47fbfac3a6248c33997c295922eb6497
Adrien Faure <adrien.faure@protonmail.com>
2026-02-24 11:23:23 +0100

    nixos: create swapfile for noco

--- nixos/deployments/hardware-noco.nix
+++ nixos/deployments/hardware-noco.nix
@@ -30,7 +30,12 @@
     options = ["fmask=0077" "dmask=0077"];
   };
 
-  swapDevices = [];
+  swapDevices = [
+   {
+      device = "/swapfile";
+      size = (1024 * 16); # RAM size = 16 G
+    }
+  ];
 
   # Enables DHCP on each ethernet and wireless interface. In case of scripted networking
   # (the default) this is the recommended approach. When using systemd-networkd it's
