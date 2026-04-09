9ed3f2c5d71d392e4a3b60bb5ea09e93f25a5623
Adrien Faure <adrien.faure@protonmail.com>
2026-04-09 22:47:18 +0200

    refacto home modules

--- flake.nix
+++ flake.nix
@@ -50,47 +50,10 @@
       ];
       systems = ["x86_64-linux" "aarch64-linux" "aarch64-darwin" "x86_64-darwin"];
       perSystem = {
-        config,
-        self',
-        inputs',
         pkgs,
-        system,
         ...
       }: {
-        # Per-system attributes can be defined here. The self' and inputs'
-        # module parameters provide easy access to attributes of the same
-        # system.
-
-        # packages.myVscode = inputs'.nixos-unstable.pkgs.callPackage ./pkgs/vscode {};
-        # packages.nix = inputs'.nixos-unstable.nix;
         formatter = pkgs.alejandra;
       };
-      flake = {
-        # The usual flake attributes can be defined here, including system-
-
-        # agnostic ones like nixosModule and system-enumerating ones, although
-        # those are more easily expressed in perSystem.
-      };
     };
-
-  # outputs = {
-  #   nixpkgs,
-  #   nixos-unstable,
-  # }: let
-  #   system = "x86_64-linux";
-  #   pkgs = import nixpkgs {
-  #     inherit system;
-  #     config.allowUnfree = true;
-  #     overlays = [];
-  #   };
-  #   unstable = import nixos-unstable {
-  #     inherit system;
-  #     config.allowUnfree = true;
-  #     overlays = [];
-  #   };
-  # in {
-  #   packages.${system} = rec {
-
-  #   };
-  # };
 }
