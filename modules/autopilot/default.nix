1d934a86b73c57b6ad441797891d63f3cb5fdfbf
Adrien Faure <adrien.faure@protonmail.com>
2026-05-26 22:34:45 +0200

    flake: wip: draft of nixosConfiguration

--- modules/autopilot/default.nix
+++ modules/autopilot/default.nix
@@ -1,5 +1,4 @@
-{ inputs, lib, ... }: {
-
+{ inputs, lib, nixpkgs, getSystem, ... }: {
   # Import flake modules
   imports = (lib.mapAttrsToList (name: _: ../flake + "/${name}") (lib.readDir ../flake));
 
@@ -15,5 +14,14 @@
     });
   };
 
+  flake.nixosConfigurations.lune = inputs.nixpkgs.lib.nixosSystem {
+    specialArgs = {};
+    modules = [
+      inputs.determinate.nixosModules.default
+
+      ../../hosts/lune/configuration.nix
+      ../../hosts/lune/hardware.nix
+    ];
+  };
 }
 
