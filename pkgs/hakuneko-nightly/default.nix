f96fe4c1a8192827dcb92588d98048037216e288
Adrien Faure <adrien.faure@protonmail.com>
2026-06-01 09:18:54 +0200

    pkgs: fix hash hakeneko

--- pkgs/hakuneko-nightly/default.nix
+++ pkgs/hakuneko-nightly/default.nix
@@ -4,6 +4,6 @@
 pkgs.hakuneko.overrideAttrs {
   src = builtins.fetchurl {
     url = "https://github.com/manga-download/hakuneko/releases/download/nightly-20200705.1/hakuneko-desktop_8.3.4_linux_amd64.deb";
-    sha256 = "sha256-somncbvpx+atkkyztugez3k/mcnflrdpz0eflmsq4he=";
+    sha256 = "sha256:04g25b5jq1a1rx7ifba5qcqkyyfghi0vb6dcj29ycpv92mqagsa8";
   };
 }
