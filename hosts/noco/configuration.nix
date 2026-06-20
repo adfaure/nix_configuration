39f533709ffa0efb1db91926944baa735d96296b
Adrien Faure <adrien.faure@protonmail.com>
2026-06-20 12:05:22 +0200

    nixos: module by host

--- hosts/noco/configuration.nix
+++ hosts/noco/configuration.nix
@@ -2,4 +2,17 @@
   networking.hostName = "noco";
   time.timeZone = "Europe/Paris";
   system.stateVersion = "24.11"; # Did you read the comment?
+
+  nixosModules.cachix.enable = true;
+  nixosModules.minimal.enable = true;
+  nixosModules.flakes.enable = true;
+  nixosModules.graphical.enable = true;
+  nixosModules.guix.enable = true;
+  nixosModules.syncthing.enable = true;
+  nixosModules.vm.enable = true;
+  nixosModules.user.enable = true;
+  nixosModules.adfaure.enable = true;
+
+  nixosModules.gnome.enable = true;
+  nixosModules.niri.enable = false;
 }
