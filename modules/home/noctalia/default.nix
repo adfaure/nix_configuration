f5a4b21c75cd1bc8716e66433e6dee7a4f2ab3dd
Adrien Faure <adrien.faure@protonmail.com>
2026-06-20 12:10:39 +0200

    flake: fmt

--- modules/home/noctalia/default.nix
+++ modules/home/noctalia/default.nix
@@ -1,63 +1,63 @@
-{ lib, inputs, ... }:
-{ config
-, pkgs
-, ...
-}:
-
-let
+{
+  lib,
+  inputs,
+  ...
+}: {
+  config,
+  pkgs,
+  ...
+}: let
   cfg = config.homeManagerModules.noctalia;
   inherit (lib) mkEnableOption;
-in
-{
-  imports = [ inputs.noctalia.homeModules.default ];
+in {
+  imports = [inputs.noctalia.homeModules.default];
 
   options.homeManagerModules.noctalia = {
     enable = mkEnableOption "noctalia";
   };
 
   config = lib.mkIf cfg.enable {
+    home.packages = with pkgs; [
+      brightnessctl
+      cliphist
+      ddcutil
+      wl-clipboard
+      wireplumber
+    ];
 
-      home.packages = with pkgs; [
-        brightnessctl
-        cliphist
-        ddcutil
-        wl-clipboard
-        wireplumber
-      ];
+    home.sessionVariables.QT_QPA_PLATFORMTHEME = "gtk3";
 
-      home.sessionVariables.QT_QPA_PLATFORMTHEME = "gtk3";
+    gtk = {
+      enable = true;
+      gtk4.theme = config.gtk.theme;
 
-      gtk = {
-        enable = true;
-        gtk4.theme = config.gtk.theme;
-
-        theme = {
-          package = pkgs.nordic;
-          name = "Nordic";
-        };
-
-        iconTheme = {
-          package = pkgs.nordzy-icon-theme;
-          name = "Nordzy-dark";
-        };
-
-        cursorTheme = {
-          package = pkgs.nordzy-cursor-theme;
-          name = "Nordzy-cursors";
-          size = 24;
-        };
+      theme = {
+        package = pkgs.nordic;
+        name = "Nordic";
       };
 
-      home.pointerCursor = {
-        size = 24;
+      iconTheme = {
+        package = pkgs.nordzy-icon-theme;
+        name = "Nordzy-dark";
+      };
+
+      cursorTheme = {
         package = pkgs.nordzy-cursor-theme;
         name = "Nordzy-cursors";
-        gtk.enable = true;
-        x11.enable = true;
-      };
-
-      programs.noctalia-shell.enable = true;
-      programs.noctalia-shell.settings = {
+        size = 24;
       };
     };
+
+    home.pointerCursor = {
+      size = 24;
+      package = pkgs.nordzy-cursor-theme;
+      name = "Nordzy-cursors";
+      gtk.enable = true;
+      x11.enable = true;
+    };
+
+    programs.noctalia-shell.enable = true;
+    programs.noctalia-shell.settings = {
+    };
+  };
 }
